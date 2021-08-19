2df9fd2a87c3852df285910b599e20f1ae9c3a21
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-20 02:45:03 +0600

    practice

--- js/map-and-filter.js
+++ js/map-and-filter.js
@@ -1,3 +1,4 @@
+// map
 let arr = [2, 3, 4, 5];
 let arrSqr = arr.map((x) => {
   return x ** 2;
@@ -5,6 +6,7 @@ let arrSqr = arr.map((x) => {
 
 console.log(arrSqr);
 
+// filter
 let arr2 = [21, 312, 41, 52, 23, 112, 5, 32];
 let arrFilter = arr2.filter((x) => {
   return x < 50;
@@ -12,3 +14,11 @@ let arrFilter = arr2.filter((x) => {
 
 console.log(arrFilter);
 console.log(arrFilter.sort());
+
+// map
+let foods = ['cake', 'coffee', 'burger'];
+let newFoods = foods.map((element, x, y) => {
+  return `I love ${element} ${x} ${y}`;
+});
+
+console.log(newFoods);
