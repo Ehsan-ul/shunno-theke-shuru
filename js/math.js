958cb5262c8417751acc6085f4bf6e01064c2e3b
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-20 22:40:34 +0600

    math

---
+++ js/math.js
@@ -0,0 +1,36 @@
+console.log(Math.E);
+console.log(Math.PI);
+
+// absolute number
+let n = 5.214;
+console.log(Math.abs(n));
+
+// floor and ceil
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+
+// round number
+console.log('===== round number ====');
+
+console.log(Math.round(n));
+console.log(Math.round(6.49));
+console.log(Math.round(6.51));
+
+// max, min
+console.log('==== max min ====');
+
+console.log(Math.max(300, 210, 790, 48.3, 890.32));
+console.log(Math.min(300, 210, 790, 48.3, 890.32));
+
+// power, sruare root
+console.log('==== power, square root ====');
+
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(49));
+
+// random number
+console.log('==== random numebrs ====');
+
+console.log(Math.random());
+console.log(Math.random() * 50);
+console.log(Math.round(Math.random() * 50));
